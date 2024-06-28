6d17e109ec720ae4e4ee0404e63250b5fb466bc5
EnesOzeren <enesozeren7@gmail.com>
2024-06-28 11:59:20 +0200

    os path join is added instead of hardcoded paths

--- mlops_project/predict_model.py
+++ mlops_project/predict_model.py
@@ -1,8 +1,10 @@
+import os
 import torch
 import argparse
 from transformers import BertForSequenceClassification, BertTokenizer
 from utils.utils_functions import read_dataset
 from datetime import datetime
+
 
 def predict(model_path: str, dataset_path: str) -> None:
     """Run prediction for a given model and dataset.
@@ -14,14 +16,11 @@ def predict(model_path: str, dataset_path: str) -> None:
     """
 
     # Load the tokenizer
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
     # Load the model
     model = BertForSequenceClassification.from_pretrained(
-        'bert-base-uncased', 
-        num_labels=2, 
-        output_attentions=False, 
-        output_hidden_states=False
+        "bert-base-uncased", num_labels=2, output_attentions=False, output_hidden_states=False
     )
 
     # Load the model weights
@@ -29,15 +28,15 @@ def predict(model_path: str, dataset_path: str) -> None:
     model.eval()
 
     # Set the device to GPU if available
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
     # Read the dataset
     tweets, _ = read_dataset(dataset_path)
 
     # Tokenize the dataset
-    inputs = tokenizer(tweets, return_tensors='pt', padding=True, truncation=True, max_length=512)
-    
+    inputs = tokenizer(tweets, return_tensors="pt", padding=True, truncation=True, max_length=512)
+
     # Move inputs to the device
     inputs = {key: val.to(device) for key, val in inputs.items()}
 
@@ -50,18 +49,18 @@ def predict(model_path: str, dataset_path: str) -> None:
 
     return probabilities
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     # Arguments
-    parser = argparse.ArgumentParser(description='Prediction')
-    parser.add_argument('--model_path', type=str, help='Path to model')
-    parser.add_argument('--dataset_path', type=str, help='Path to dataset')
+    parser = argparse.ArgumentParser(description="Prediction")
+    parser.add_argument("--model_path", type=str, help="Path to model")
+    parser.add_argument("--dataset_path", type=str, help="Path to dataset")
     args = parser.parse_args()
 
     # Run prediction
     predictions = predict(args.model_path, args.dataset_path)
 
     # Save predictions
-    current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    PREDICTION_PATH = f'outputs/predictions/predictions_{current_time}.pt'
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    PREDICTION_PATH = os.path.join("outputs", "predictions", f"predictions_{current_time}.pt")
     torch.save(predictions, PREDICTION_PATH)
